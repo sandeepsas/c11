d5ad0639e21cc23c247d7784dcf1e2a3022b3e75
Sandeep Sasidharan <sasidhar.sandeep@gmail.com>
2017-03-06 10:44:24 -0500

    Update testVectorFind.cpp

--- testVectorFind.cpp
+++ testVectorFind.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+bool isSandeep(string str);
+
 int main(){
 
 	vector<string> v;
@@ -27,6 +29,36 @@ int main(){
         std::cout << "v does not contain: " << val << '\n';
     }
 
+
+    auto index2 = find_if(begin(v),end(v), isSandeep);
+
+	if (index2 != std::end(v)) {
+        std::cout << "v contains: " << *index2 << '\n';
+    } else {
+        std::cout << "v does not contain: Sandeep" << '\n';
+    }
+
+
+    auto index3 = find_if(begin(v),end(v), [](string str){
+    	return (str == "dog");
+    });
+
+    if (index3 != std::end(v)) {
+        std::cout << "v contains: " << *index3 << '\n';
+    } else {
+        std::cout << "v does not contain: Dog" << '\n';
+    }
+
+
 	return 1;
 
 }
+
+bool isSandeep(string str){
+
+	if(str == "Sandeep")
+		return true;
+
+	return false;
+}
+
